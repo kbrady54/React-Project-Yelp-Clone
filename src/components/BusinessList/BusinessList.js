cf0b08e55daa38e511c47197a74d6148109e77aa
Kyle Brady <kbrady54@gmail.com>
2018-12-11 11:59:17 -0500

    Implement Yelp API and finish project

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -4,10 +4,11 @@ import Business from "../Business/Business";
 
 class BusinessList extends React.Component {
   render() {
+    console.log(this.props.businesses);
     return (
       <div className="BusinessList">
         {this.props.businesses.map(business => {
-          return <Business business={business} />;
+          return <Business business={business} key={business.id} />;
         })}
       </div>
     );
